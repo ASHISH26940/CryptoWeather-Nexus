e86a4a65de6b32d1047b742698d0fc1a7283f828
Ashish Gupta <gupta.ashish2694@gmail.com>
2025-04-03 19:44:21 +0530

    news-cryo=pto-weather api added backend

---
+++ backend/src/routes/crypto.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import {getCryptoPrice} from "../controllers/crypto/crypto"
+const router= express.Router();
+// ✅ Enable JSON body parsing
+router.use(express.json());
+
+router.get("/currencies",getCryptoPrice);
+
+export default router;
---
+++ backend/src/routes/weather.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import {geoLoaction} from "../controllers/weather/weather"
+const router= express.Router();
+// ✅ Enable JSON body parsing
+router.use(express.json());
+
+router.post("/location/lat_lon",geoLoaction);
+
+export default router;
